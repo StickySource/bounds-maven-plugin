4d61da758c80c2f6ae357782a47880b4ab2fd4cf
Michael McCallum <michael@StickyCode.net>
2020-07-27 22:43:34 +1200

    Merge the artifacts and coordinates into one list and process it

---
+++ src/main/java/net/stickycode/plugin/bounds/ArtifactLookup.java
@@ -0,0 +1,49 @@
+package net.stickycode.plugin.bounds;
+
+import org.eclipse.aether.artifact.DefaultArtifact;
+
+public class ArtifactLookup {
+
+  private DefaultArtifact artifact;
+
+  private String gav;
+
+  private String propertyName;
+
+  public ArtifactLookup() {
+  }
+
+  public ArtifactLookup with(DefaultArtifact defaultArtifact) {
+    this.artifact = defaultArtifact;
+    if (propertyName == null)
+      this.propertyName = defaultArtifact.getArtifactId() + ".version";
+
+    return this;
+  }
+
+  public DefaultArtifact getArtifact() {
+    return artifact;
+  }
+
+  public ArtifactLookup withGav(String gav) {
+    this.gav = gav;
+    return this;
+  }
+
+  public ArtifactLookup withPropertyName(String property) {
+    this.propertyName = property;
+    return this;
+  }
+
+  public ArtifactLookup(String gav) {
+    this.gav = gav;
+  }
+
+  public String getGav() {
+    return gav;
+  }
+
+  public String getPropertyName() {
+    return propertyName;
+  }
+}

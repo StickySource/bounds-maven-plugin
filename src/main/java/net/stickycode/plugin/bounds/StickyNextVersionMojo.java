b8d7cf72a95cf4c0e150bb22c5ec76e0814f738c
Michael McCallum <michael@StickyCode.net>
2020-07-29 18:54:15 +1200

    Increments and deriving range to lookup and all good

--- src/main/java/net/stickycode/plugin/bounds/StickyNextVersionMojo.java
+++ src/main/java/net/stickycode/plugin/bounds/StickyNextVersionMojo.java
@@ -6,6 +6,7 @@ import java.time.Clock;
 import java.time.Instant;
 import java.util.List;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -27,17 +28,44 @@ import org.eclipse.aether.version.Version;
 public class StickyNextVersionMojo
     extends AbstractMojo {
 
-  @Parameter(defaultValue = "minor", readonly = true)
+  /**
+   * Choose the rule to use in incrementing the version:
+   *
+   * <dl>
+   * <dt>major</dt>
+   * <dd>Increment the major version e.g. 1.2 -> 2.1, 20 -> 21.1</dd>
+   * <dt>minor</dt>
+   * <dd>Increment the second component e.g. 1.2 -> 1.3, 1 -> 1.1, 20.50 -> 20.51</dd>
+   * <dt>patch</dt>
+   * <dd>Increment the third component e.g. 1.2 -> 1.2.1, 1.2.1 -> 1.2.2</dd>
+   * <dt>patchDatetime</dt>
+   * <dd>Set the third component to current time in seconds e.g. 1.2 -> 1.2.123456789, 1.2.1 -> 1.2.123456789, 1 ->
+   * 1.1.123456789</dd>
+   * </dl>
+   */
+  @Parameter(defaultValue = "minor", required = true)
   private VersionIncrement versionIncrement = VersionIncrement.minor;
 
-  @Parameter(defaultValue = "nextVersion", readonly = true)
+  /**
+   * The name of the property in the session to set to the next value
+   */
+  @Parameter(defaultValue = "nextVersion", required = true)
   private String nextVersionProperty;
+
+  /**
+   * If the current maven project should be set to the next version, only affects the build session not the pom.xml.
+   */
+  @Parameter(defaultValue = "true", required = true)
+  private boolean updateProjectVersion;
 
   /**
    * The current repository/network configuration of Maven.
    */
   @Parameter(defaultValue = "${repositorySystemSession}", readonly = true)
-  private RepositorySystemSession session;
+  private RepositorySystemSession repositorySession;
+
+  @Parameter(defaultValue = "${session}")
+  protected MavenSession mavenSession;
 
   @Parameter(defaultValue = "${project.remoteProjectRepositories}", readonly = true)
   private List<RemoteRepository> repositories;
@@ -51,28 +79,54 @@ public class StickyNextVersionMojo
   @Override
   public void execute()
       throws MojoExecutionException, MojoFailureException {
-    Version version = highestVersion();
+    String nextVersion = nextVersion(project.getVersion());
 
-    String nextVersion = increment(version.toString());
-    project.getProperties().setProperty(nextVersionProperty, version.toString());
+    project.getProperties().setProperty(nextVersionProperty, nextVersion);
     log("set %s to %s", nextVersionProperty, nextVersion);
+
+    if (updateProjectVersion) {
+      project.setVersion(nextVersion);
+      project.getBuild().setFinalName(project.getArtifactId() + "-" + nextVersion);
+      log("set project version to %s", nextVersion);
+    }
   }
 
-  String increment(String version) throws MojoFailureException {
+  String nextVersion(String projectVersion) {
+    String versionRange = versionRange(projectVersion);
+
+    Version version = highestVersion(versionRange);
+
+    String nextVersion = increment(version.toString());
+    return nextVersion;
+  }
+
+  String increment(String version) {
     String[] components = version.toString().split("\\.");
     switch (getVersionIncrement()) {
       case major:
         return String.join(".", Integer.toString(valueOf(components[0]) + 1), "1");
+
       case minor:
+        if (components.length < 2)
+          return String.join(".", components[0], "1");
+
         return String.join(".", components[0], Integer.toString(valueOf(components[1]) + 1));
+
       case patch:
+        if (components.length < 3)
+          return String.join(".", components[0], components[1], "1");
+
         return String.join(".", components[0], components[1], Integer.toString(valueOf(components[2]) + 1));
+
       case patchDatetime:
+        if (components.length < 2)
+          return String.join(".", components[0], "1", Long.toString(Instant.now(getClock()).getEpochSecond()));
+
         return String.join(".", components[0], components[1], Long.toString(Instant.now(getClock()).getEpochSecond()));
 
     }
 
-    throw new MojoFailureException("Unknown version increment " + getVersionIncrement());
+    throw new RuntimeException("Unknown version increment " + getVersionIncrement());
   }
 
   VersionIncrement getVersionIncrement() {
@@ -96,16 +150,13 @@ public class StickyNextVersionMojo
         return "[" + components[0] + "," + (valueOf(components[0]) + 1) + ")";
 
     }
-    // String[] components = project.getVersion().split(":");
-    // if (components.length < 1)
-    // throw new MojoFailureException("Version " + project.getVersion() + " does not have an obvious major version component");
-    //
-    return components[0];
+
+    throw new RuntimeException("Unknown version increment " + getVersionIncrement());
   }
 
-  Version highestVersion() {
-    DefaultArtifact artifact = new DefaultArtifact(project.getGroupId(), project.getArtifactId(), project.getPackaging(),
-      versionRange(project.getVersion()));
+  Version highestVersion(String versionRange) {
+    DefaultArtifact artifact = projectArtifact(versionRange);
+
     VersionRangeRequest request = new VersionRangeRequest(artifact, repositories, null);
     VersionRangeResult v = resolve(request);
 
@@ -115,12 +166,22 @@ public class StickyNextVersionMojo
         : new RuntimeException("Failed to resolve " + artifact.toString(), v.getExceptions().get(0));
     }
 
+    log("resolved %s:%s:%s to %s", artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion(), v.getHighestVersion());
     return v.getHighestVersion();
   }
 
-  private VersionRangeResult resolve(VersionRangeRequest request) {
+  DefaultArtifact projectArtifact(String versionRange) {
+    DefaultArtifact artifact = new DefaultArtifact(
+      project.getGroupId(),
+      project.getArtifactId(),
+      project.getPackaging(),
+      versionRange);
+    return artifact;
+  }
+
+  VersionRangeResult resolve(VersionRangeRequest request) {
     try {
-      return repository.resolveVersionRange(session, request);
+      return repository.resolveVersionRange(repositorySession, request);
     }
     catch (VersionRangeResolutionException e) {
       throw new RuntimeException(e);
